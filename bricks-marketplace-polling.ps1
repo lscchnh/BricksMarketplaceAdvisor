073d0fb5cb297957fa09ad651ccc70609f782bf0
Louis Cochinho <louis.cochinho-ext@soitec.com>
2022-08-02 18:00:27 +0200

    automatisation

--- bricks-marketplace-polling.ps1
+++ bricks-marketplace-polling.ps1
@@ -1,53 +1,75 @@
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("accept-Language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7")
-$headers.Add("access-Control-Request-Headers", "authorization")
-$headers.Add("access-Control-Request-Method", "GET")
-$headers.Add("origin", "https://app.bricks.co")
-$headers.Add("referer", "https://app.bricks.co/")
-$headers.Add("sec-fetch-dest", "empty")
-$headers.Add("sec-fetch-mode", "cors")
-$headers.Add("sec-fetch-site", "same-site")
-$headers.Add("sec-ch-ua", "`" Not A;Brand`";v=`"99`", `"Chromium`";v=`"99`", `"Google Chrome`";v=`"99`"")
-$headers.Add("sec-ch-ua-mobile", "?0")
-$headers.Add("sec-ch-ua-platform", "`"Windows`"")
-$headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
-$headers.Add("authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg5OTlkZDM2LWVkMjMtNGY0My1hN2MyLWM4ZGU3OTVkNmUyYyIsImV4cGlyYXRpb24iOiIyMDIyLTAzLTIzVDA3OjQ3OjU3Ljk5OFoiLCJ0b2tlblR5cGUiOiJjdXN0b21lciIsImlhdCI6MTY0NzQxNjg3NywiZXhwIjoxNjQ4MDIxNjc3fQ.BMJWnjHiouay4alTmVkwuWBC4h90T5IoJtvJ7DxlxF4")
-$response=""
+param (
+    [Parameter(Mandatory=$true)][string]$email,
+    [Parameter(Mandatory=$true)][string]$password,
+    [Parameter(Mandatory=$true)][string]$maxPrice,
+    [Parameter(Mandatory=$true)][string]$minProfitability,
+    [Parameter(Mandatory=$true)][string]$minDividend,
+    [Parameter(Mandatory=$true)][string]$maxDeltaValuation
+ )
 
-function process_error {
+ function process_error {
     param (
         $Exception
     )
 
-    #Send-MailMessage -To "louis.cochinho@hotmail.fr" -From ""  -Subject "Bricks marketplace alert" -Body 'Bricks polling Status code : $Exception.Response.StatusCode.value__' -Credential (Get-Credential) -SmtpServer "smtp server" -Port 587
+    Write-Output $Exception
 }
 
-function process_success {
-    param (
-        $response
-    )
-
-    $responseData = @($response | Select-Object -Property "data")
-
-    if($responseData.length -gt 1) 
+function get_login_token {
+    param($email, $password)
+    try
     {
-        Write-Output $responseData
+        $body = "{`"email`":`"$email`",`"password`":`"$password`"}"
+        $response = Invoke-RestMethod 'https://api.bricks.co/customers/email/sign-in' -Method 'POST' -Headers $headers -Body $body
+        $response | ConvertTo-Json    
+        $token = $response."token"
+        $headers.Add("Authorization", "Bearer $token")
     }
-    #Send-MailMessage -To "louis.cochinho@hotmail.fr" -From ""  -Subject "New bricks in marketplace" -Body 'https://app.bricks.co/marketplace' -Credential (Get-Credential) -SmtpServer "smtp server" -Port 587
+    catch
+    {
+        process_error($_.Exception)
+    }
 }
 
-while(1)
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "application/json")
+get_login_token $email $password
+$cursor=0
+$totalOffers=1
+while($cursor -le 50)
 {
     try
     {
-        $response = Invoke-RestMethod 'https://api.bricks.co/marketplace/deals?priceRange=100&priceRange=500000&profitabilityRange=5&profitabilityRange=20&dividendsRange=2&dividendsRange=15&sort=createdat_desc&take=10' -Method 'GET' -Headers $headers
+        $url = "https://api.bricks.co/marketplace/deals?priceRange=10&priceRange=$maxPrice&profitabilityRange=$minProfitability&profitabilityRange=20&dividendsRange=$minDividend&dividendsRange=15&cursor=$cursor"
+        $response = Invoke-RestMethod $url -Method 'GET' -Headers $headers
+        $response | ConvertTo-Json -Depth 5
+
+        $responseData = $response."data"
+        $totalOffers = $response."total"."offers"
+        Write-Output $responseData.length
+        if($responseData.length -ge 1) 
+        {
+            Foreach($i in $responseData)
+            {
+                $deltaValuation = $i."performance"."deltaValuation"
+                Write-Output "deltaValuation = $deltaValuation"
+                if($deltaValuation -lt $maxDeltaValuation)
+                {
+                    $arr += @($i."property"."id")
+                }
+            }
+        }
+
+        $cursor+=10
     }
     catch
     {
         process_error($_.Exception)
     }
 
-    process_success($response | ConvertTo-Json)
+    #Send-MailMessage -To "louis.cochinho@hotmail.fr" -From ""  -Subject "New bricks in marketplace" -Body 'https://app.bricks.co/marketplace' -Credential (Get-Credential) -SmtpServer "smtp server" -Port 587
+    
     start-sleep -seconds 2
 }
 
+Write-Output "arr=$arr"
