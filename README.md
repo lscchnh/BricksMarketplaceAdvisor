fd70f4b553403cef91a945077830f130e4fbc702
Louis Cochinho <louis.cochinho@viseo.com>
2022-11-23 14:18:34 +0100

    v3

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Introduction
 
-The script is useful if you want to be notified by mail of offers in the [Bricks.co](https://www.bricks.co) marketplace. You can use filters to get informed of offers corresponding to your desired criterias.
+The script is useful if you want to be notified by mail of offers in the [Bricks.co](https://www.bricks.co) marketplace. You can use filters to get informed of offers corresponding to your desired criterias. You can choose to be notified of the best offer or a list of offers matching your criterias.
 
 # Getting started
 
@@ -15,14 +15,15 @@ Download the latest release and run the powershell script with the beyond argume
 
 ```ps
     [Parameter(Mandatory=$true)][string]$bricksEmail,
-    [Parameter(Mandatory=$true)][string]$bricksPassword,
+    [Parameter(Mandatory=$true)][string]$bricksPwd,
     [Parameter(Mandatory=$true)][string]$email,
-    [Parameter(Mandatory=$true)][string]$emailPassword,
+    [Parameter(Mandatory=$true)][string]$emailPwd,
     [Parameter(Mandatory=$true)][string]$smtpServer,
-    [Parameter(Mandatory=$true)][string]$maxPrice,
-    [Parameter(Mandatory=$true)][string]$minProfitability,
-    [Parameter(Mandatory=$true)][string]$minDividend,
-    [Parameter(Mandatory=$true)][string]$maxDeltaValuation
+    [Parameter(Mandatory=$false)][string]$maxPrice = "500000",
+    [Parameter(Mandatory=$false)][string]$minProfitability = "5",
+    [Parameter(Mandatory=$false)][string]$minDividend = "2",
+    [Parameter(Mandatory = $false)][string]$maxPriceVariation = "5",
+    [Parameter(Mandatory = $false)][bool]$getMinPriceVariation = $false
 ```
 
 **/!\ maxPrice is in cents.**
--- bricks-marketplace-advisor.ps1
+++ bricks-marketplace-advisor.ps1
@@ -1,16 +1,25 @@
 param (
-    [Parameter(Mandatory=$true)][string]$bricksEmail,
-    [Parameter(Mandatory=$true)][string]$bricksPassword,
-    [Parameter(Mandatory=$true)][string]$email,
-    [Parameter(Mandatory=$true)][string]$emailPassword,
-    [Parameter(Mandatory=$true)][string]$smtpServer,
-    [Parameter(Mandatory=$true)][string]$maxPrice,
-    [Parameter(Mandatory=$true)][string]$minProfitability,
-    [Parameter(Mandatory=$true)][string]$minDividend,
-    [Parameter(Mandatory=$true)][string]$maxDeltaValuation
- )
+    [Parameter(Mandatory = $true)][string]$bricksEmail,
+    [Parameter(Mandatory = $true)][string]$bricksPwd,
+    [Parameter(Mandatory = $true)][string]$email,
+    [Parameter(Mandatory = $true)][string]$emailPwd,
+    [Parameter(Mandatory = $true)][string]$smtpServer,
+    [Parameter(Mandatory = $false)][string]$maxPrice = "500000",
+    [Parameter(Mandatory = $false)][string]$minProfitability = "5",
+    [Parameter(Mandatory = $false)][string]$minDividend = "2",
+    [Parameter(Mandatory = $false)][string]$maxPriceVariation = "5",
+    [Parameter(Mandatory = $false)][bool]$getMinPriceVariation = $false
+)
 
- function process_error {
+function Initialize-Variables {
+    $script:cursor = 10
+    $script:totalOffers = 1
+    $script:currentMinPriceVariation = 100
+    $script:currentMinPriceVariationUrl = $null
+    $script:currentMinPriceVariationPropertyName = $null
+}
+
+function Process_Error {
     param (
         $Exception
     )
@@ -18,79 +27,81 @@ param (
     Write-Output $Exception
 }
 
-function get_login_token {
-    param($bricksEmail, $bricksPassword)
-    try
-    {
-        $body = "{`"email`":`"$bricksEmail`",`"password`":`"$bricksPassword`"}"
+function Get-Token {
+    param($bricksEmail, $bricksPwd)
+    try {
+        $body = "{`"email`":`"$bricksEmail`",`"password`":`"$bricksPwd`"}"
         $response = Invoke-RestMethod 'https://api.bricks.co/customers/email/sign-in' -Method 'POST' -Headers $headers -Body $body 
         $token = $response."token"
         $headers.Add("Authorization", "Bearer $token")
     }
-    catch
-    {
-        process_error($_.Exception)
+    catch {
+        Process_Error $_.Exception
     }
 }
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Content-Type", "application/json")
 
-get_login_token $bricksEmail $bricksPassword
+Get-Token $bricksEmail $bricksPwd
+Initialize-Variables
 
-$cursor = 10
-$totalOffers = 1
-
-while($true)
-{
-    try
-    {
-        $filters="priceRange=1000&priceRange=$maxPrice&profitabilityRange=$minProfitability&profitabilityRange=20&dividendsRange=$minDividend&dividendsRange=15&cursor=$cursor"
+while ($true) {
+    try {
+        $filters = "priceRange=100&priceRange=$maxPrice&profitabilityRange=$minProfitability&profitabilityRange=20&dividendsRange=$minDividend&dividendsRange=15&cursor=$cursor"
         $url = "https://api.bricks.co/marketplace/deals?$filters"
         $response = Invoke-RestMethod $url -Method 'GET' -Headers $headers
         $responseData = $response."data"
         $totalOffers = $response."total"."offers"
-        $processing = [Math]::Truncate($cursor*100/$totalOffers)
-        if($responseData.length -ge 1) 
-        {                      
-            Foreach($i in $responseData)
-            {
-                $deltaValuation = $i."performance"."brickPriceVariationWithCommission"                
-                if($deltaValuation -lt $maxDeltaValuation)
-                {
-                    Write-Output "Found !!!"
-                    $arr += @("https://app.bricks.co/marketplace?$filters")
+        $processing = [Math]::Truncate($cursor * 100 / $totalOffers)
+        if ($responseData.length -ge 1) {                      
+            Foreach ($i in $responseData) {
+                $brickPriceVariation = $i."brickPriceVariation"
+                $propertyName = $i."property"."name".Replace(" ", "%20")       
+                if ($brickPriceVariation -lt $maxPriceVariation) {                   
+                    if ($getMinPriceVariation) {
+                        if ($brickPriceVariation -lt $currentMinPriceVariation) {
+                            $currentMinPriceVariation = $brickPriceVariation
+                            $currentMinPriceVariationUrl = @("https://app.bricks.co/marketplace?priceRange=100&priceRange=$maxPrice&profitabilityRange=$minProfitability&profitabilityRange=20&dividendsRange=$minDividend&dividendsRange=15&sort=profitability_desc&searchField=$propertyName")
+                        }
+                    }
+                    else {
+                        Write-Output "Found !!!"
+                        $arr += @("https://app.bricks.co/marketplace?$filters&sort=profitability_desc")   
+                    }
                 }
             }
         }
 
         Write-Output "$cursor offers processed over $totalOffers ($processing%)"
         
-        $cursor+=10
+        $cursor += 10
         start-sleep -seconds 1
     }
-    catch
-    {
-        process_error($_.Exception)
+    catch {
+        Process_Error $_.Exception
     }
     
-    if($cursor -ge $totalOffers)
-    {
-        if($arr.length -gt 0)
-        {
-            [securestring]$secStringPassword = ConvertTo-SecureString $emailPassword -AsPlainText -Force
-            $credential = New-Object System.Management.Automation.PSCredential ($email, $secStringPassword)
-            Send-MailMessage -To "$email" -From "$email"  -Subject "New bricks in marketplace" -Body "$arr" -UseSsl -Credential $credential -SmtpServer "$smtpServer" -Port 587
-            $arr=$null
+    if ($cursor -ge $totalOffers) {
+        [securestring]$secStringPassword = ConvertTo-SecureString $emailPwd -AsPlainText -Force
+        $credential = New-Object System.Management.Automation.PSCredential ($email, $secStringPassword)
+
+        if ($arr.length -gt 0) {
+            Send-MailMessage -To "$email" -From "$email"  -Subject "New bricks in marketplace (maxPrice=$maxPrice minProfitability=$minProfitability minDividend=$minDividend maxPriceVariation=$maxPriceVariation)" -Body "$arr" -UseSsl -Credential $credential -SmtpServer "$smtpServer" -Port 587
+            
             Write-Output "Email sent to $email."         
         }
-        else
-        {
+        elseif ($getMinPriceVariation -and -not ($null -eq $currentMinPriceVariationUrl)) {
+            Send-MailMessage -To "$email" -From "$email"  -Subject "[BEST DELTA VALUATION] New bricks in marketplace (maxPrice=$maxPrice minProfitability=$minProfitability minDividend=$minDividend maxPriceVariation=$maxPriceVariation)" -Body "$currentMinPriceVariationUrl" -UseSsl -Credential $credential -SmtpServer "$smtpServer" -Port 587
+
+            Write-Output "Email sent to $email."  
+        }
+        else {
             Write-Output "No offers found..."
         }
 
-        Write-Output "Waiting 5 minutes."
+        Write-Output "Waiting 5 minutes."       
         start-sleep -seconds 300
-        $cursor = 10       
+        Initialize-Variables
     }
 }
